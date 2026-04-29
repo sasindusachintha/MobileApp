62a8a2defa4122824185a4fcfe68df7930d9e2d5
sasindusachintha <sasindusachintha1234@gmail.com>
2026-04-29 10:25:54 +0530

    add

---
+++ today/app/src/main/java/com/example/sasinew/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.sasinew;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnctn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnctn = findViewById(R.id.btnctn);
+
+        btnctn.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, Login.class));
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
